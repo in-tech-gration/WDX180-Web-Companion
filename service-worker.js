993e01d03fe3398a1e1b807186bde632d4d0458b
Kostas Minaidis <minaidisk@gmail.com>
2025-06-16 20:28:47 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -22,12 +22,13 @@ chrome.sidePanel
 chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
   if (!tab.url) return;
   const url = new URL(tab.url);
+
   // Enables the side panel on google.com
   if (url.origin === GOOGLE_ORIGIN) {
     await chrome.sidePanel.setOptions({
       tabId,
       path: 'sidepanel.html',
-      enabled: true
+      enabled: true,
     });
   } else {
     // Disables the side panel on all other sites
